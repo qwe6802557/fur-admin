37c25cba9a71f52d678196b4c04f8f19333eb6fe
qwe6802557 <425160813@qq.com>
2020-01-07 00:18:25 +0800

    add-dialog-styles changes

--- fur-front/src/components/contact/contact-add-dialog/index.vue
+++ fur-front/src/components/contact/contact-add-dialog/index.vue
@@ -4,6 +4,7 @@
       <div class="left-content">
         <i class="el-icon-search"></i>
         <el-input class="contact-input" v-model="search"  placeholder="search user..." @input="searchList" ></el-input>
+        <el-scrollbar wrap-class="main-top-scroll" :native="false" wrap-style="height:500px;" view-style="" view-class="view-box" ref="scrollBar">
         <div class="left-list">
           <el-checkbox-group v-model="checkedList" @change="handleCheckedListChange">
             <ul>
@@ -14,11 +15,23 @@
             </ul>
           </el-checkbox-group>
         </div>
+        </el-scrollbar>
       </div>
     </div>
-    <div class="add-dialog-right">
-      <i class="el-icon-close" @click="addClose"></i>
 
+    <div class="add-dialog-right" style="color: black;">
+      <i class="el-icon-close" @click="addClose"></i>
+      <div class="title">请勾选需要添加的好友</div>
+      <el-scrollbar wrap-class="main-top-scroll" :native="false" wrap-style="height:500px;" view-style="" view-class="view-box" ref="scrollBar">
+        <div class="left-list">
+            <ul>
+              <li v-for="(item,index) in boxList">
+                <img src="../../../../static/images/logo.png" alt="">
+                <el-checkbox  :label="item.id" :key="item.id" disabled>{{item.user_name}}</el-checkbox>
+              </li>
+            </ul>
+        </div>
+      </el-scrollbar>
     </div>
   </div>
 </template>
@@ -30,7 +43,16 @@
           const list = ['上海','北京','北京','北京']
           return {
             search:'',
-            boxList:[{id:1,img:'',user_name:'张三'},{id:2,img:'',user_name:'李四'},{id:3,img:'',user_name:'王五'}],
+            boxList:[{id:1,img:'',user_name:'张三'},
+              {id:2,img:'',user_name:'李四'},{
+              id:3,img:'',user_name:'王五'},
+              {id:4,img:'',user_name:'王五'},
+              {id:5,img:'',user_name:'王五'},
+              {id:6,img:'',user_name:'王五'},
+              {id:7,img:'',user_name:'王五'},
+              {id:8,img:'',user_name:'王五'},
+              {id:9,img:'',user_name:'王五'}
+            ],
             checkedList:[]
           }
         },
